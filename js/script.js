fb42a079e56203a862117480e16698d76cad0ea7
Marcos Hayama <marcos.hayama@gmail.com>
2023-05-15 05:43:56 -0300

    js incompleto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-const accordionContent = document.querySelectorAll(".accordion");
+const accordion_item = document.querySelectorAll(".accordion_item");
 
-accordionContent.forEach((item, index) => {
-    let header = item.querySelector(".accordion-header");
-    header.addEventListener("click", () => {
-        item.classList.toggle("open");
+accordion_item.forEach((item) => {
+  const accordion_header_item = item.querySelector(".accordion_header");
 
-        let description = item.querySelectorAll(".matriz-descricao");
-        if(item.classList.contains("open")){
-            description.style.height = `${description.scrollHeight}px`;
-         }else{
-            description.style.height = "0px";
-         }
-    })
-    console.log(item)
-})
+  accordion_header_item.addEventListener("click", () => {
+    const accordion_content_item = item.querySelector(".accordion_content");
+
+    const item_actived = document.querySelector(".active");
+
+    VerifyActive(item, accordion_content_item, item_actived);
+  });
+});
+
+function VerifyActive(item, content, content_actived) {
+  const icon_item = item.querySelector(".icon");
+  const icon_item_active = document.querySelectorAll(".icon");
+
+  icon_item_active.forEach((item) => (item.innerHTML = "+"));
+
+  if (content_actived) {
+    content_actived.style.height = 0;
+    content_actived.classList.remove("active");
+  }
+
+  if (content !== content_actived) {
+    icon_item.innerHTML = "-";
+    content.classList.add("active");
+    content.style.height = content.scrollHeight + 10 + "px";
+  }
+}
